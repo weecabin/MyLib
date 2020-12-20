7c4728839d878c76bed3702a26883b6cce17d9f1
play.js <git@gituser.com>
2020-12-20 08:14:22 -0800

    added myfs and tests for it

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const mylib=require("./xmlFlightplan")
+const myfs=require("./myfs")
 
+var printHold = ""
+const print = (msg) => {
+  printHold+=msg;
+}
+const println = (msg) => {
+  console.log(printHold+msg);
+  printHold="";
+}
+
+/*
 let node = new mylib.Node("root","value","attr=\"attr value\"")
 node.AddChild(new mylib.Node("child1","child1 value"))
 console.log (node.ToXML())
+*/
+myfs.mkdir("../../Flightplans/KSFO")
+println("")
+myfs.mkdir("../../Flightplans/KSFO/fp.fpl")
+println("")
+myfs.mkdir("./fp.fpl")
---
+++ myfs.js
@@ -0,0 +1,69 @@
+const fs = require("fs")
+
+var printHold = ""
+const print = (msg) => {
+  printHold+=msg;
+}
+const println = (msg) => {
+  console.log(printHold+msg);
+  printHold="";
+}
+
+/*
+if the passed in directory exists, does nothing
+of it doesnt, it creates it, checking each folder as it goes.
+supports ./dir1/dir1  or  ../../dir1/dir2
+*/
+function mkdir(path)
+{
+  println("mkdir "+path)
+  // split the path by folder, then check the existance of each one and create as needed
+  let split = path.split("/")
+  //println(split)
+  let dir=""
+  let sofar=""
+  for (dir of split)
+  {
+    //println(dir) 
+    let dotpos = dir.indexOf(".")
+    let dotindir = dotpos>=0?true:false
+    if (dotpos>0)
+    {
+      println("exiting, filename found: "+dir)
+      return
+    }
+    if (sofar.length==0)
+      sofar = dir
+    else 
+      sofar+="/"+dir
+    if (!dotindir)
+    {
+      println("check: "+sofar)
+      if(!fs.existsSync(sofar))
+      {
+        let createErr=false
+        /*
+        fs.mkdirSync(sofar, 0766, function(err)
+        {
+          if(err)
+          {
+            createErr=true
+            console.log(err);
+            // echo the result back
+            response.send("ERROR! Can't make directory! "+sofar+"\n");
+          }
+        });*/
+        if(!createErr) println(sofar+" created");
+      }
+      else
+      {
+        println(sofar+" already exists")
+      }
+    }
+  }
+}
+
+module.exports =
+{
+  mkdir:mkdir
+}
